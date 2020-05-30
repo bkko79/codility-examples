3ac4ad091e1750427516fff49cf3fd0deb888cc5
kuki <bkko@outlook.kr>
2020-05-30 14:38:35 +0900

    logic fixed

--- longestBinaryGap.js
+++ longestBinaryGap.js
@@ -1,13 +1,12 @@
 function longestBinaryGap(N) {
   // Error if not positive
-  if (N <= 0) return null
-
-  const binaryNum = N.toString(2);
-  const binaryGaps = binaryNum.slice(binaryNum.indexOf('1') + 1, binaryNum.lastIndexOf('1'));
-  const zeroCounted = binaryGaps.split('1').map(zeros => zeros.length);
-  console.log(zeroCounted)
-
-  return zeroCounted.length ? Math.max(...zeroCounted) : 0;
+  if (N < 0) return null;
+  
+  const binary = N.toString(2)
+  const between = binary.slice(binary.indexOf('1')+1, binary.lastIndexOf('1'))
+  
+  const gaps = between.split('1').map(zeros => zeros.length)
+  return gaps.length ? Math.max(...gaps) : 0
 }
 
-//console.log(longestBinaryGap(21474647));
+console.log(longestBinaryGap(21474647));
