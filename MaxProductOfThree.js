19b65b1f588a0e9a9faea8b58602963ba232a8a0
kuki <bkko@outlook.kr>
2020-05-30 22:36:49 +0900

    MaxProductOfThree.js > sort logic readability fix

--- MaxProductOfThree.js
+++ MaxProductOfThree.js
@@ -1,5 +1,5 @@
 function MaxProductOfThree(A){
-  A.sort((a, b) => {return a-b});
+  A.sort((a, b) => a-b );
 
   const n = A[A.length-1] * A[A.length-2] * A[A.length-3];
   const m = A[0] * A[1] * A[A.length-1];
