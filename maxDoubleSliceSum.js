fda813de36157e88453d3151d195f8050bcd5c1c
kuki <bkko@outlook.kr>
2020-05-30 14:19:30 +0900

    maxDoubleSliceSum finished

--- maxDoubleSliceSum.js
+++ maxDoubleSliceSum.js
@@ -1,18 +1,24 @@
 function maxDoubleSliceSum(A){
-  let k1, k2 = []
+  const N = A.length;
 
-  for (let i = 1; i < A.length-1; i++){
-    k1[i] = Math.max(k1[i-1] + A[i], 0)
-  }
-  for (let j = A.length-2; j > 0; j--){
-    k2[j] = Math.max(k2[j+1]+A[j], 0)
+  if(N === 3) return 0;
+
+  let headSum = A.map(i => 0);
+  let tailSum = A.map(i => 0);
+
+  for(let i = 1; i < N-1; i++) {
+      headSum[i] = Math.max(0, headSum[i-1] + A[i]);
   }
 
-  let max = 0
-  for (let m = 1; m < A.length-1; m++){
-    max = Math.max(max, k1[m-1]+k2[m+1])
+  for(let i = N-2; i >= 1; i--) {
+      tailSum[i] = Math.max(0, tailSum[i+1] + A[i]);
   }
-  return max
+
+  for(let i = 1; i < N-1; i++) {
+      maxSum = Math.max(maxSum, headSum[i-1] + tailSum[i+1]);    
+  } 
+
+  return maxSum;
 }
 
 console.log(maxDoubleSliceSum([3,2,6,-1,4,5,-1,2]))
